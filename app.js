bcc58a4433c512b61a8c09748156f72a589cbbb2
rmamchyk <roman.mamchyk@gmail.com>
2020-02-08 23:00:10 +0200

    added animation for log list

--- app.js
+++ app.js
@@ -4,42 +4,46 @@ new Vue({
         playerHealth: 100,
         monsterHealth: 100,
         gameIsRunning: false,
-        turns: []
+        turns: [],
+        currentTurn: 0
     },
     methods: {
-        startGame: function() {
+        startGame: function () {
             this.gameIsRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
             this.turns = [];
         },
-        attack: function() {
+        attack: function () {
             var damage = this.calculateDamage(3, 10);
             this.monsterHealth -= damage;
             this.turns.unshift({
                 isPlayer: true,
-                text: 'Player hits Monster for ' + damage
+                text: 'Player hits Monster for ' + damage,
+                id: this.currentTurn + 1
             });
+            this.currentTurn++;
             if (this.checkWin()) {
                 return;
             }
 
             this.monsterAttacks();
         },
-        specialAttack: function() {
+        specialAttack: function () {
             var damage = this.calculateDamage(10, 20);
             this.monsterHealth -= damage;
             this.turns.unshift({
                 isPlayer: true,
-                text: 'Player hits Monster hard for ' + damage
+                text: 'Player hits Monster hard for ' + damage,
+                id: this.currentTurn + 1
             });
+            this.currentTurn++;
             if (this.checkWin()) {
                 return;
             }
-
             this.monsterAttacks();
         },
-        heal: function() {
+        heal: function () {
             if (this.playerHealth <= 90) {
                 this.playerHealth += 10;
             } else {
@@ -47,35 +51,39 @@ new Vue({
             }
             this.turns.unshift({
                 isPlayer: true,
-                text: 'Player heals for 10'
+                text: 'Player heals for 10',
+                id: this.currentTurn + 1
             });
+            this.currentTurn++;
             this.monsterAttacks();
         },
-        giveUp: function() {
+        giveUp: function () {
             this.gameIsRunning = false;
         },
         monsterAttacks: function() {
             var damage = this.calculateDamage(5, 12);
             this.playerHealth -= damage;
+            this.checkWin();
             this.turns.unshift({
                 isPlayer: false,
-                text: 'Monster hits Player for ' + damage
+                text: 'Monster hits Player for ' + damage,
+                id: this.currentTurn + 1
             });
-            this.checkWin();
+            this.currentTurn++;
         },
         calculateDamage: function(min, max) {
             return Math.max(Math.floor(Math.random() * max) + 1, min);
         },
         checkWin: function() {
             if (this.monsterHealth <= 0) {
-                if (confirm('You won! New game?')) {
+                if (confirm('You won! New Game?')) {
                     this.startGame();
                 } else {
                     this.gameIsRunning = false;
                 }
                 return true;
             } else if (this.playerHealth <= 0) {
-                if (confirm('You lost! New game?')) {
+                if (confirm('You lost! New Game?')) {
                     this.startGame();
                 } else {
                     this.gameIsRunning = false;
@@ -85,4 +93,4 @@ new Vue({
             return false;
         }
     }
-})
+});
